b76cf63d6244cfe4dd1fa387ebcb2e496db6feb1
NadirAgalarov <141878898+NadirAgalarov@users.noreply.github.com>
2024-06-22 16:28:00 +0400

    new

---
+++ src/main/java/com/news/haray_hakathon/HarayHakathonApplication.java
@@ -0,0 +1,13 @@
+package com.news.haray_hakathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarayHakathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarayHakathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/news/haray_hakathon/HarayHakathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news.haray_hakathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarayHakathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
